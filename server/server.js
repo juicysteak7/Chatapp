f2e3577aacb462c6b610898564982f5887b8f241
juicysteak7 <83801343+juicysteak7@users.noreply.github.com>
2022-03-08 12:26:27 -0800

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const { Server } = require("socket.io")
 const mongoose = require("mongoose")
 
 const Message = require("./models/Message")
+const User = require("./models/Users")
 require("dotenv").config()
 
 
@@ -33,9 +34,9 @@ const io = new Server(server, {
 io.on("connection", (socket)=>{
     console.log(`User Connected: ${socket.id}`)
 
-    socket.on("join",(room,id)=>{
+    socket.on("join",(room/*,id*/)=>{
         socket.join(room)
-        socket.join(id)
+        //socket.join(id)
         console.log(`User connected to room: ${room}`)
     })
 
@@ -54,10 +55,38 @@ io.on("connection", (socket)=>{
                 if(datum.room === room){
                     console.log("sending message...")
                     console.log(id)
-                    socket.to(id).emit("receive_message",datum)
+                    io.to(id).emit("receive_message",resultum)
                 }
             })
         })
+    })
+
+    socket.on("check_users", (username, pw, id)=>{
+        User.find().then(result=>{
+            result.map((resultum)=>{
+                const datum = {
+                    username: resultum.username,
+                    password: resultum.password,
+                }
+                console.log(datum)
+                if(datum.username === username && datum.password === pw){
+                    console.log("confirming user")
+                    io.to(id).emit("confirm_user")
+                }
+            })
+        })
+    })
+
+    socket.on("new_user",(username, pw)=>{
+       
+        const newUser = new User({
+            username: username,
+            password: pw
+        })
+
+        newUser.save()
+
+        console.log("User saved")
     })
 
     socket.on("send_message",(data)=>{
