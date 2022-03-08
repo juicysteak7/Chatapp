1d7e1bb3d25e65053caaf59ae0be832888668025
juicysteak7 <83801343+juicysteak7@users.noreply.github.com>
2022-03-08 12:25:48 -0800

    Add files via upload

---
+++ server/models/Users.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        ref:"username",
+    },
+    password:{
+        type:String,
+        required:true,
+        ref:"password",
+    },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
