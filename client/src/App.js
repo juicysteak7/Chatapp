3205b3d1109f8af119b03c9f364894e4dcff5890
juicysteak7 <83801343+juicysteak7@users.noreply.github.com>
2022-03-08 12:24:29 -0800

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,43 @@
 import './App.css';
 import io from "socket.io-client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Chat from "./Chat"
 
 const socket = io.connect("http://localhost:8000")
 
 function App() {
-  const [username, setUsername] =useState("");
+  const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
   const [showChat,setShowChat] = useState(false);
+  const [valid,setValid] = useState(false);
+  const [pw, setPw] = useState("");
 
   const joinRoom = () => {
-    if(username !== "" && room !== ""){
-      socket.emit("join", room, socket.id);
+    if(username !== "" && room !== "" && valid){
+      socket.emit("join", room/*, socket.id*/);
       setShowChat(true);
+    }else{
+      console.log("User confirmation unsuccessful")
+    }
+  }
+  const newUser = () => {
+    if(username !== "" && pw !== ""){
+      socket.emit("new_user", username, pw);
+      setValid(true)
+    }
+  }
+  const confirmUser = () => {
+    if(username !== "" && pw !== ""){
+      socket.emit("check_users", username, pw, socket.id)
     }
   }
 
+  useEffect(() => {
+      socket.on("confirm_user", ()=>{
+          console.log("User confirmed")
+          setValid(true)
+      })
+  },[socket]);
 
   return (
     <div className="App">
@@ -32,12 +53,21 @@ function App() {
           />
           <input 
             type="text"
+            placeholder="Enter Your Password"
+            onChange={(e)=>{
+              setPw(e.target.value);
+            }}
+          />
+          <input 
+            type="text"
             placeholder="Enter the Room ID"
             onChange={(e)=>{
               setRoom(e.target.value);
             }}
           />
           <button onClick={joinRoom}>Join a Room</button>
+          <button onClick={confirmUser}>Confirm User</button>
+          <button onClick={newUser}>New User</button>
         </div>
       ):(
         <Chat socket={socket} username={username} room={room}/>
